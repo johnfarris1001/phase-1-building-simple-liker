75bd4f64c614f9105345601e836abc0ea65fafd3
John <johnfarris217@gmail.com>
2022-12-30 15:15:47 -0600

    commit tree 90f98be19b2a962469615a60acc24ab9b0be76a2

--- main.js
+++ main.js
@@ -4,16 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector("#modal");
+modal.className = "hidden";
 
+const likeHearts = document.getElementsByClassName("like-glyph");
+
+for (const glyph of likeHearts) {
+  glyph.addEventListener("click", () => {
+    mimicServerCall()
+      .then(() => {
+        if (glyph.className === "like-glyph") {
+          glyph.innerHTML = FULL_HEART;
+          glyph.className = "like-glyph activated-heart";
+        } else {
+          glyph.innerHTML = EMPTY_HEART;
+          glyph.className = "like-glyph";
+        }
+
+      })
+      .catch(() => {
+        modal.className = "";
+        setTimeout(() => modal.className = "hidden", 3000)
+      });
+  });
+}
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
